3fe5e0cb5e85ec950181ba16c5ef31472b1d38ae
acostapazo <costapazo@gmail.com>
2019-10-28 11:10:08 +0100

    Solved a bug on correlation_id_provider for flask applications

--- petisco/frameworks/flask/correlation_id_provider.py
+++ petisco/frameworks/flask/correlation_id_provider.py
@@ -4,7 +4,11 @@ from petisco.controller.correlation_id import CorrelationId
 def flask_correlation_id_provider(func_name: str = "") -> CorrelationId:
     try:
         import connexion
-        correlation_id = CorrelationId(connexion.request.headers.get("X-Correlation-Id"))
+        correlation_id_str = connexion.request.headers.get("X-Correlation-Id")
+        if correlation_id_str:
+            correlation_id = CorrelationId(correlation_id_str)
+        else:
+            correlation_id = CorrelationId.generate(func_name)
     except ImportError:
         correlation_id = CorrelationId.generate(func_name)
     return correlation_id
