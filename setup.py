2badf6746c24eeac0e87f100b017a91d2e2c3a72
acostapazo <acosta@alicebiometrics.com>
2020-02-07 09:03:47 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -49,9 +49,10 @@ setup(
     include_package_data=True,
     zip_safe=False,
     extras_require={
-        "flask": ["connexion[swagger-ui]==2.2.0", "Flask-Cors==3.0.7"],
-        "sqlalchemy": ["sqlalchemy>=1.3.11", "sqlalchemy_utils"],
-        "redis": ["redis >= 3.3.11", "fakeredis >= 1.0.5"],
+        "flask": ["connexion==2.2.0", "connexion[swagger-ui]", "Flask-Cors==3.0.7"],
+        "sqlalchemy": ["sqlalchemy>=1.3.11", "sqlalchemy_utils", "PyMySQL==0.9.2"],
+        "redis": ["redis >= 3.3.11", "fakeredis>=1.0.5"],
         "rabbitmq": ["pika==1.1.0"],
+        "gunicorn": ["gunicorn", "json-logging-py==0.2"]
     },
 )
