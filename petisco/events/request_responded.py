70b6594725aa80c3113b43a5417f79bc98c2562e
acostapazo <costapazo@gmail.com>
2020-03-24 20:38:14 +0100

    Update RequestResponded Domain Event version

--- petisco/events/request_responded.py
+++ petisco/events/request_responded.py
@@ -12,7 +12,25 @@ REQUEST_RESPONDED_UNWRAP_ERROR = (
     },
 )
 
-REQUEST_RESPONDED_VERSION = "1.1.0"
+REQUEST_RESPONDED_VERSION = "1.2.0"
+
+
+def is_flask_response(value):
+    try:
+        from flask import Response
+
+        if isinstance(value, Response):
+            return True
+    except (RuntimeError, ImportError):
+        pass
+    return False
+
+
+def get_content(value):
+    if is_flask_response(value):
+        return "flask response"
+    else:
+        return json.dumps(value)
 
 
 class RequestResponded(Event):
@@ -52,17 +70,10 @@ class RequestResponded(Event):
             "status_code": 500,
         }
         if isinstance(http_response, Tuple):
-            _http_response["content"] = json.dumps(http_response[0])
+            _http_response["content"] = get_content(http_response[0])
             _http_response["status_code"] = http_response[1]
         else:
-            try:
-                from flask import Response
-
-                if isinstance(http_response, Response):
-                    _http_response["content"] = "flask response (multipart)"
-                    _http_response["status_code"] = http_response.status_code
-
-            except (RuntimeError, ImportError):
-                pass
+            _http_response["content"] = get_content(http_response)
+            _http_response["status_code"] = http_response.status_code
 
         self.http_response = _http_response
---
+++ tests/unit/events/test_request_responded.py
@@ -0,0 +1,76 @@
+import pytest
+from flask import Response
+
+from petisco.events.request_responded import RequestResponded
+
+
+@pytest.fixture
+def given_tuple_http_response():
+    return {"message": "ok"}, 200
+
+
+@pytest.fixture
+def given_tuple_flask_http_response():
+    return Response(), 200
+
+
+@pytest.fixture
+def given_tuple_flask_response():
+    return Response()
+
+
+@pytest.mark.unit
+def test_should_construct_request_responded_from_a_tuple_http_response(
+    given_tuple_http_response
+):
+
+    request_responded = RequestResponded(
+        application="petisco",
+        controller="my-controller",
+        is_success=True,
+        http_response=given_tuple_http_response,
+        correlation_id="correlation-id",
+        elapsed_time=1.0,
+        additional_info=None,
+    )
+
+    assert request_responded.http_response["content"] == '{"message": "ok"}'
+    assert request_responded.http_response["status_code"] == 200
+
+
+@pytest.mark.unit
+def test_should_construct_request_responded_from_a_tuple_flask_http_response(
+    given_tuple_flask_http_response
+):
+
+    request_responded = RequestResponded(
+        application="petisco",
+        controller="my-controller",
+        is_success=True,
+        http_response=given_tuple_flask_http_response,
+        correlation_id="correlation-id",
+        elapsed_time=1.0,
+        additional_info=None,
+    )
+
+    assert request_responded.http_response["content"] == "flask response"
+    assert request_responded.http_response["status_code"] == 200
+
+
+@pytest.mark.unit
+def test_should_construct_request_responded_from_flask_http_response(
+    given_tuple_flask_response
+):
+
+    request_responded = RequestResponded(
+        application="petisco",
+        controller="my-controller",
+        is_success=True,
+        http_response=given_tuple_flask_response,
+        correlation_id="correlation-id",
+        elapsed_time=1.0,
+        additional_info=None,
+    )
+
+    assert request_responded.http_response["content"] == "flask response"
+    assert request_responded.http_response["status_code"] == 200
