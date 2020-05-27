81fd263d2775539ded52abd382e26b20067b3858
Francisco Javier Garcia Salomon <fgarcia@alicebiometrics.com>
2020-05-27 13:19:59 +0200

    Fix bug when configuring Slack Notifier

--- petisco/notifier/config/config_notifier.py
+++ petisco/notifier/config/config_notifier.py
@@ -27,7 +27,7 @@ def get_slack_notifier():
     return SlackNotifier(
         token=slack_token,
         channel=slack_channel,
-        converter=SlackNotifierMessageConverter,
+        converter=SlackNotifierMessageConverter(),
     )
 
 
