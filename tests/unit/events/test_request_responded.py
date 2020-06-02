84e699ccc0cf350bd8b12a8bf1b47d3ef1407db7
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-06-02 16:15:12 +0200

    Update RequestResponded http content managing

--- tests/unit/events/test_request_responded.py
+++ tests/unit/events/test_request_responded.py
@@ -10,6 +10,16 @@ def given_tuple_http_response():
 
 
 @pytest.fixture
+def given_tuple_http_response_with_long_content():
+    return {"message": "ok" * 300}, 200
+
+
+@pytest.fixture
+def given_tuple_http_response_with_long_content_and_406():
+    return {"error": {"type": "MyError", "message": "no" * 300}}, 406
+
+
+@pytest.fixture
 def given_tuple_flask_http_response():
     return Response(), 200
 
@@ -23,7 +33,6 @@ def given_tuple_flask_response():
 def test_should_construct_request_responded_from_a_tuple_http_response(
     given_tuple_http_response, given_any_info_id
 ):
-
     request_responded = RequestResponded(
         app_name="petisco",
         app_version="1.0.0",
@@ -34,15 +43,59 @@ def test_should_construct_request_responded_from_a_tuple_http_response(
     ).add_info_id(given_any_info_id)
 
     assert request_responded.event_name == "request.responded"
-    assert request_responded.http_response["content"] == {"message": "Response OK"}
+    assert request_responded.http_response["content"] == {"message": "ok"}
     assert request_responded.http_response["status_code"] == 200
+
+
+@pytest.mark.unit
+def test_should_construct_request_responded_from_a_tuple_http_response_with_long_content(
+    given_tuple_http_response_with_long_content, given_any_info_id
+):
+    request_responded = RequestResponded(
+        app_name="petisco",
+        app_version="1.0.0",
+        controller="my-controller",
+        is_success=True,
+        http_response=given_tuple_http_response_with_long_content,
+        elapsed_time=1.0,
+    ).add_info_id(given_any_info_id)
+
+    assert request_responded.event_name == "request.responded"
+    assert request_responded.http_response["content"] == {
+        "message": "Response OK (Trimmed message: message too long)",
+        "message_size": 615,
+    }
+    assert request_responded.http_response["status_code"] == 200
+
+
+@pytest.mark.unit
+def test_should_construct_request_responded_from_a_tuple_http_response_with_long_content_and_406(
+    given_tuple_http_response_with_long_content_and_406, given_any_info_id
+):
+    request_responded = RequestResponded(
+        app_name="petisco",
+        app_version="1.0.0",
+        controller="my-controller",
+        is_success=True,
+        http_response=given_tuple_http_response_with_long_content_and_406,
+        elapsed_time=1.0,
+    ).add_info_id(given_any_info_id)
+
+    assert request_responded.event_name == "request.responded"
+    assert request_responded.http_response["content"] == {
+        "error": {
+            "type": "MyError",
+            "message": "Response Error (Trimmed message: message too long)",
+            "message_size": 645,
+        }
+    }
+    assert request_responded.http_response["status_code"] == 406
 
 
 @pytest.mark.unit
 def test_should_construct_request_responded_from_a_tuple_flask_http_response(
     given_tuple_flask_http_response, given_any_info_id
 ):
-
     request_responded = RequestResponded(
         app_name="petisco",
         app_version="1.0.0",
@@ -60,7 +113,6 @@ def test_should_construct_request_responded_from_a_tuple_flask_http_response(
 def test_should_construct_request_responded_from_flask_http_response(
     given_tuple_flask_response, given_any_info_id
 ):
-
     request_responded = RequestResponded(
         app_name="petisco",
         app_version="1.0.0",
