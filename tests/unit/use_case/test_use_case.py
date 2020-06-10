888a549927b2da8d945b1a24d8f9625907fd9ea8
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-06-10 15:58:05 +0200

    Update handler tests

--- tests/unit/use_case/test_use_case.py
+++ tests/unit/use_case/test_use_case.py
@@ -3,7 +3,7 @@ from typing import NamedTuple
 import pytest
 from meiga.assertions import assert_failure, assert_success
 
-from petisco import UseCase, use_case_handler, INFO, ERROR, DEBUG
+from petisco import UseCase, use_case_handler, INFO, ERROR, DEBUG, UnknownError
 from meiga import Success, Failure, isFailure, isSuccess, Error
 
 from tests.unit.mocks.fake_logger import FakeLogger
@@ -318,13 +318,20 @@ def test_should_log_successfully_an_error_raised_by_a_meiga_handler():
 def test_should_use_case_handler_return_a_failure_with_unknown_error_when_raise_an_uncontrolled_exception():
 
     logger = FakeLogger()
+    expected_raised_exception = RuntimeError("uncontrolled exception")
 
     @use_case_handler(logger=logger)
     class MyUseCase(UseCase):
         def execute(self):
-            raise RuntimeError("uncontrolled exception")
+            raise expected_raised_exception
 
-    MyUseCase().execute()
+    result = MyUseCase().execute()
+
+    assert_failure(
+        result,
+        value_is_instance_of=UnknownError,
+        value_is_equal_to=UnknownError(expected_raised_exception),
+    )
 
     first_logging_message = logger.get_logging_messages()[0]
     second_logging_message = logger.get_logging_messages()[1]
