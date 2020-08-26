740e47d4cc7ce4aa65b74f29c198c276bb0006f6
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-08-26 16:58:19 +0200

    Add testing on logging level configuration and fix a minor bug

--- petisco/application/singleton.py
+++ petisco/application/singleton.py
@@ -5,3 +5,9 @@ class Singleton(type):
         if cls not in cls._instances:
             cls._instances[cls] = super(Singleton, cls).__call__(*args, **kwargs)
         return cls._instances[cls]
+
+    def clear(cls):
+        try:
+            del Singleton._instances[cls]
+        except KeyError:
+            pass
