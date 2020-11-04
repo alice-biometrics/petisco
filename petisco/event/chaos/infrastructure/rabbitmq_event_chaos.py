0f0620d6b0531a4568a4ece6242145b05a241963
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-11-04 15:38:00 +0100

    Fix bizarre errors when configure RabbitMqEventChaos

--- petisco/event/chaos/infrastructure/rabbitmq_event_chaos.py
+++ petisco/event/chaos/infrastructure/rabbitmq_event_chaos.py
@@ -22,15 +22,9 @@ EVENT_CHAOS_PERCENTAGE_SIMULATE_FAILURES_KEY = (
 class RabbitMqEventChaos(IEventChaos):
     def __init__(
         self,
-        percentage_simulate_nack: Optional[float] = os.environ.get(
-            EVENT_CHAOS_PERCENTAGE_SIMULATE_NACK_KEY
-        ),
-        delay_before_even_handler_second: Optional[float] = os.environ.get(
-            EVENT_CHAOS_DELAY_BEFORE_EVENT_HANDLER_SECONDS_KEY
-        ),
-        percentage_simulate_failures: Optional[float] = os.environ.get(
-            EVENT_CHAOS_PERCENTAGE_SIMULATE_FAILURES_KEY
-        ),
+        percentage_simulate_nack: Optional[float] = None,
+        delay_before_even_handler_second: Optional[float] = None,
+        percentage_simulate_failures: Optional[float] = None,
     ):
         """
         Parameters
@@ -42,9 +36,38 @@ class RabbitMqEventChaos(IEventChaos):
         percentage_simulate_failures
             Percentage of simulate failures [0.0 -> 1.0]. Where 1.0 simulate always a failure on handlers.
         """
-        self.percentage_simulate_nack = float(percentage_simulate_nack)
-        self.delay_before_even_handler_second = float(delay_before_even_handler_second)
-        self.percentage_simulate_failures = float(percentage_simulate_failures)
+        self._set_percentage_simulate_nack(percentage_simulate_nack)
+        self._set_delay_before_even_handler_second(delay_before_even_handler_second)
+        self._set_percentage_simulate_failures(percentage_simulate_failures)
+
+    def _set_percentage_simulate_nack(self, percentage_simulate_nack):
+        if percentage_simulate_nack is None:
+            percentage_simulate_nack = os.environ.get(
+                EVENT_CHAOS_PERCENTAGE_SIMULATE_NACK_KEY
+            )
+        self.percentage_simulate_nack = self._float(percentage_simulate_nack)
+
+    def _set_delay_before_even_handler_second(self, delay_before_even_handler_second):
+        if delay_before_even_handler_second is None:
+            delay_before_even_handler_second = os.environ.get(
+                EVENT_CHAOS_DELAY_BEFORE_EVENT_HANDLER_SECONDS_KEY
+            )
+        self.delay_before_even_handler_second = self._float(
+            delay_before_even_handler_second
+        )
+
+    def _set_percentage_simulate_failures(self, percentage_simulate_failures):
+        if percentage_simulate_failures is None:
+            percentage_simulate_failures = os.environ.get(
+                EVENT_CHAOS_PERCENTAGE_SIMULATE_FAILURES_KEY
+            )
+        self.percentage_simulate_failures = self._float(percentage_simulate_failures)
+
+    def _float(self, value):
+        return float(value) if value is not None else None
+
+    def __repr__(self):
+        return f"RabbitMqEventChaos: {self.info()}"
 
     def info(self) -> Dict:
         return {
--- tests/modules/event/integration/test_rabbitmq_event_chaos.py
+++ tests/modules/event/integration/test_rabbitmq_event_chaos.py
@@ -11,12 +11,61 @@ from petisco.event.chaos.domain.event_chaos_error import EventChaosError
 
 
 @pytest.mark.integration
+def test_should_configure_with_envvars(monkeypatch):
+    monkeypatch.setenv("EVENT_CHAOS_PERCENTAGE_SIMULATE_NACK", "1.0")
+    monkeypatch.setenv("EVENT_CHAOS_DELAY_BEFORE_EVENT_HANDLER_SECONDS", "1.0")
+    monkeypatch.setenv("EVENT_CHAOS_PERCENTAGE_SIMULATE_FAILURES", "1.0")
+
+    chaos = RabbitMqEventChaos()
+
+    assert chaos.info() == {
+        "EVENT_CHAOS_PERCENTAGE_SIMULATE_NACK": 1.0,
+        "EVENT_CHAOS_DELAY_BEFORE_EVENT_HANDLER_SECONDS": 1.0,
+        "EVENT_CHAOS_PERCENTAGE_SIMULATE_FAILURES": 1.0,
+    }
+
+
+@pytest.mark.integration
 def test_should_create_default_rabbitmq_event_chaos():
     chaos = RabbitMqEventChaos()
 
     assert chaos.nack_simulation(None, None) is False
     chaos.delay()
     assert chaos.simulate_failure_on_result(isSuccess) == isSuccess
+
+
+@pytest.mark.integration
+def test_should_create_default_rabbitmq_event_chaos_with_env_percentage_simulate_nack_1(
+    monkeypatch
+):
+    monkeypatch.setenv("EVENT_CHAOS_PERCENTAGE_SIMULATE_NACK", "1.0")
+
+    chaos = RabbitMqEventChaos()
+
+    mock_channel = Mock(BlockingChannel)
+    mock_method = Mock(Basic.Deliver)
+    mock_method.delivery_tag = Mock(Any)
+
+    assert chaos.nack_simulation(mock_channel, mock_method) is True
+    chaos.delay()
+    assert chaos.simulate_failure_on_result(isSuccess) == isSuccess
+
+    mock_channel.basic_nack.assert_called_once()
+
+
+@pytest.mark.integration
+def test_should_create_default_rabbitmq_event_chaos_with_env_percentage_simulate_failures_1(
+    monkeypatch
+):
+    monkeypatch.setenv("EVENT_CHAOS_PERCENTAGE_SIMULATE_FAILURES", "1.0")
+    monkeypatch.setenv("EVENT_CHAOS_DELAY_BEFORE_EVENT_HANDLER_SECONDS", "0.2")
+
+    chaos = RabbitMqEventChaos()
+    assert chaos.nack_simulation(None, None) is False
+    chaos.delay()
+    assert chaos.simulate_failure_on_result(isSuccess) == Failure(
+        EventChaosError(Exception())
+    )
 
 
 @pytest.mark.integration
