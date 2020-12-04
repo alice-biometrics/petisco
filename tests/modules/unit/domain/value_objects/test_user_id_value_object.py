64be1344c064550125d3c27309bd7bfb5ac232ae
Artur Costa-Pazo <acosta@alicebiometrics.com>
2020-12-04 15:55:13 +0100

    Update UserId to deal with legacy formats

--- tests/modules/unit/domain/value_objects/test_user_id_value_object.py
+++ tests/modules/unit/domain/value_objects/test_user_id_value_object.py
@@ -4,20 +4,33 @@ from meiga.assertions import assert_failure, assert_success
 from meiga.decorators import meiga
 
 from petisco.domain.value_objects.user_id import UserId
-from petisco.domain.errors.length_limit_string_value_object_error import (
-    ExceedLengthLimitValueObjectError,
-)
 from petisco.domain.value_objects.uuid import InvalidUuidError
 
 
 @pytest.mark.unit
-def test_should_declare_a_user_id_manually():
+@pytest.mark.parametrize(
+    "value", ["0VAnhfnTtMJk3SBN", "9f74b5c0-2196-4a27-a03f-cb92d66e2bbd"]
+)
+def test_should_declare_a_user_id_manually(value):
 
-    value = "9f74b5c0-2196-4a27-a03f-cb92d66e2bbd"
     user_id = UserId(value)
 
     assert isinstance(user_id, UserId)
     assert user_id.value == value
+
+
+@pytest.mark.unit
+@pytest.mark.parametrize(
+    "value", ["user_id", "my_user_id_is_too_long_for_default_limit_length"]
+)
+def test_should_fail_user_id_when_declare_a_non_valid_user_id(value):
+    @meiga
+    def controller():
+        user_id = UserId(value)
+        return Success(user_id)
+
+    result = controller()
+    assert_failure(result, value_is_instance_of=InvalidUuidError)
 
 
 @pytest.mark.unit
@@ -49,12 +62,6 @@ def test_should_declare_a_legacy_user_id_generated_automatically():
 
 
 @pytest.mark.unit
-def test_should_fail_when_declare_a_legacy_user_id_that_exceeds_default_length_limits():
-    with pytest.raises(ExceedLengthLimitValueObjectError):
-        UserId.from_legacy("my_user_id_is_too_long_for_default_limit_length")
-
-
-@pytest.mark.unit
 def test_should_fail_when_declare_a_user_id_that_exceeds_default_length_limits():
     with pytest.raises(InvalidUuidError):
         UserId("my_user_id_is_too_long_for_default_limit_length")
@@ -68,25 +75,3 @@ def test_should_return_success_user_id_if_generate_user_id_on_meiga_decorated_me
 
     result = controller()
     assert_success(result, value_is_instance_of=UserId)
-
-
-@pytest.mark.unit
-def test_should_fail_user_id_when_declare_a_non_valid_user_id():
-    @meiga
-    def controller():
-        user_id = UserId("my_user_id_is_too_long_for_default_limit_length")
-        return Success(user_id)
-
-    result = controller()
-    assert_failure(result, value_is_instance_of=InvalidUuidError)
-
-
-@pytest.mark.unit
-def test_should_fail_legacy_user_id_when_declare_a_non_valid_user_id():
-    @meiga
-    def controller():
-        user_id = UserId.from_legacy("my_user_id_is_too_long_for_default_limit_length")
-        return Success(user_id)
-
-    result = controller()
-    assert_failure(result, value_is_instance_of=ExceedLengthLimitValueObjectError)
