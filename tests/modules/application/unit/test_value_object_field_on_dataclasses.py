90b257374d9452212f7eba6e6b848f98e58bd69f
Artur Costa-Pazo <acosta@alicebiometrics.com>
2021-02-02 13:23:14 +0100

    Add option to value_object_field (default)

---
+++ tests/modules/application/unit/test_value_object_field_on_dataclasses.py
@@ -0,0 +1,23 @@
+import pytest
+from dataclasses import dataclass
+from dataclasses_json import dataclass_json
+
+from petisco import value_object_field, Uuid
+
+
+@pytest.mark.unit
+def test_should_construct_a_dataclass_with_value_object_field():
+    @dataclass_json
+    @dataclass
+    class MyDataclass:
+        name: str
+        id_: Uuid = value_object_field(value_object_class=Uuid, default=None)
+
+    my_dataclass = MyDataclass("my_name")
+    assert my_dataclass.to_json() == '{"name": "my_name", "id_": null}'
+
+    my_dataclass = MyDataclass("my_name", Uuid("f34dfc97-dd54-4b67-ae92-f88d7dbd2237"))
+    assert (
+        my_dataclass.to_json()
+        == '{"name": "my_name", "id_": "f34dfc97-dd54-4b67-ae92-f88d7dbd2237"}'
+    )
