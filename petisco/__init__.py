fd3dc02dcb55f07361537533b1b4eb0e316281c6
Artur Costa-Pazo <acosta@alicebiometrics.com>
2021-07-23 14:07:30 +0200

    Fix: remove a breakpoint

--- petisco/__init__.py
+++ petisco/__init__.py
@@ -4,9 +4,6 @@ ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 __version__ = open(f"{ROOT_PATH}/VERSION", "r").read()[:-1].rstrip()
 
-import pdb
-
-pdb.set_trace()
 from petisco import public_api
 from petisco.public_api import *
 
