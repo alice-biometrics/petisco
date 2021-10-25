0209b83c242daa4ffd49d00b05f07f50dd9189a9
Artur Costa-Pazo <acosta@alicebiometrics.com>
2021-10-25 12:11:45 +0200

    ENH: Add DEPLOY file to check deploy time. Implement DeployChecker application service

--- petisco/__init__.py
+++ petisco/__init__.py
@@ -4,6 +4,8 @@ ROOT_PATH = os.path.abspath(os.path.dirname(__file__))
 
 __version__ = open(f"{ROOT_PATH}/VERSION", "r").read()[:-1]
 
+__deploy_timestamp__ = open(f"{ROOT_PATH}/DEPLOY", "r").read()[:-1]
+
 from petisco.public_api import *
 from petisco import public_api
 
