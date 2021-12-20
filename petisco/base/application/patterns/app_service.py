aef6e0d8315e4431674da262cc3a29db37d1397b
Francisco Rodeño <frodeno@alicebiometrics.com>
2021-12-20 17:14:31 +0100

    Fix linter format

--- petisco/base/application/patterns/app_service.py
+++ petisco/base/application/patterns/app_service.py
@@ -4,6 +4,7 @@ from meiga import NotImplementedMethodError, Result
 
 from petisco.base.misc.interface import Interface
 
+
 class AppService(Interface):
     @abstractmethod
     def execute(self, *args, **kwargs) -> Result:
