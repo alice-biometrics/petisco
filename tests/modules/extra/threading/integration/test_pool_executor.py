2dd1aff62780662b66ae6d57b81e3549c0acfe71
acostapazo <acosta@alicebiometrics.com>
2022-01-24 11:42:47 +0100

    ENH: Add threading module

---
+++ tests/modules/extra/threading/integration/test_pool_executor.py
@@ -0,0 +1,24 @@
+import pytest
+
+from petisco.extra.threading import Executable, PoolExecutor
+
+
+def executable_func(value: str) -> str:
+    return value
+
+
+@pytest.mark.integration
+class TestPoolExecutor:
+    def __init__(self):
+        self.executables = [
+            Executable(executable_func, ("executable 1",)),
+            Executable(executable_func, ("executable 2",)),
+            Executable(executable_func, ("executable 3",)),
+        ]
+
+    @pytest.mark.parametrize("use_multi_threading", [True, False])
+    def should_execute_successfully(self, use_multi_threading: bool):
+        pool_executor = PoolExecutor(self.executables, use_multi_threading)
+        func_results = pool_executor.execute()
+
+        return func_results == ["executable 1", "executable 2", "executable 3"]
