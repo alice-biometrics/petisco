a59d7c27719ce24745f79b1f1913aff8db0aa41c
acostapazo <acosta@alicebiometrics.com>
2022-04-26 09:57:06 +0200

    feat: add clear_before option to rabbitmq configurer (#237)

---
+++ examples/rabbitmq/clear.py
@@ -0,0 +1,19 @@
+from examples.rabbitmq.common import ORGANIZATION, SERVICE, subscribers
+from petisco.extra.rabbitmq import RabbitMqConnector, RabbitMqMessageConfigurer
+
+
+def configure():
+    connector = RabbitMqConnector()
+
+    configurer = RabbitMqMessageConfigurer(
+        ORGANIZATION,
+        SERVICE,
+        connector,
+        # queue_config=QueueConfig(default_retry_ttl=RETRY_TTL),
+    )
+    configurer.configure_subscribers(subscribers)
+    configurer.clear()
+
+
+if __name__ == "__main__":
+    configure()
---
+++ examples/rabbitmq/configure_with_clear_before.py
@@ -0,0 +1,18 @@
+from examples.rabbitmq.common import ORGANIZATION, SERVICE, subscribers
+from petisco.extra.rabbitmq import RabbitMqConnector, RabbitMqMessageConfigurer
+
+
+def configure():
+    connector = RabbitMqConnector()
+
+    configurer = RabbitMqMessageConfigurer(
+        ORGANIZATION,
+        SERVICE,
+        connector,
+        # queue_config=QueueConfig(default_retry_ttl=RETRY_TTL),
+    )
+    configurer.configure_subscribers(subscribers, clear_before=True)
+
+
+if __name__ == "__main__":
+    configure()
