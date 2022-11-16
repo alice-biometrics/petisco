bc93141612e771e20ac8c9797aa06ad46d6f5cac
acostapazo <acosta@alicebiometrics.com>
2022-11-16 13:42:13 +0100

    chore(requirements): update elastic-apm version to 6.13.1 (#292)

--- setup.py
+++ setup.py
@@ -54,8 +54,8 @@ setup(
         "redis": ["redis==4.3.4"],
         "rabbitmq": ["pika==1.3.1"],
         "slack": ["slack_sdk==3.19.3"],
-        "elastic": ["elasticsearch<8.0.0,>=7.13.1", "elastic-apm==6.13.0"],
-        "elastic-apm": ["elastic-apm==6.13.0"],
+        "elastic": ["elasticsearch<8.0.0,>=7.13.1", "elastic-apm==6.13.1"],
+        "elastic-apm": ["elastic-apm==6.13.1"],
         "fastapi": ["fastapi==0.87.0"],
     },
 )
