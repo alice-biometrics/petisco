ff33f5cc7c2dffad1195325eae3e30e8acd38113
acostapazo <acosta@alicebiometrics.com>
2023-04-02 17:53:30 +0200

    feat(dependency): improve typing adding generic, add validation for defined builders and prepare the code to v2 migration

--- petisco/base/application/dependency_injection/dependency.py
+++ petisco/base/application/dependency_injection/dependency.py
@@ -1,37 +1,74 @@
+from __future__ import annotations
+
 import os
-from typing import Any, Dict, Union
+from typing import Any, Generic, TypeVar, get_args
 
 from petisco.base.misc.builder import Builder
 
+T = TypeVar("T")
 
-class Dependency:
+
+class Dependency(Generic[T]):
     """
     Data structure to define how our dependency (Repository, AppService, etc) is built.
 
     Definition of default Builder is Mandatory, create alternative builder with envar_modifier is optional.
     """
 
-    name: str
-    default_builder: Builder[Any]
-    envar_modifier: Union[str, None] = None
-    builders: Union[Dict[str, Builder[Any]], None] = None
+    # TODO To be deprecated as petisco will index from Generic T by default
+    name: str | None = None
+    alias: str | None = None  # Use alias instead of name to identify diferent dependencies with the same base type
+    default_builder: Builder[T] | None = None
+    # TODO, This should be  mandatory (not optional) it is temporary to help on migration to v2
+    envar_modifier: str | None = None
+    builders: dict[str, Builder[T]] | None = None
 
     def __init__(
         self,
-        name: str,
-        default_builder: Builder[Any],
-        envar_modifier: Union[str, None] = None,
-        builders: Union[Dict[str, Builder[Any]], None] = None,
+        name: str | None = None,
+        alias: str | None = None,
+        default_builder: Builder[Any] | None = None,
+        envar_modifier: str | None = None,
+        builders: dict[str, Builder[Any]] | None = None,
     ):
         self.name = name
+        self.alias = name
         self.default_builder = default_builder
         self.envar_modifier = envar_modifier
         self.builders = builders
 
-    def get_instance(self) -> Any:
+    def _validate(self) -> None:
+        generic_type = self.get_generic_type()
+        if generic_type:
+            if not issubclass(self.default_builder.klass, generic_type):
+                raise TypeError(
+                    f"Dependency: The class {self.default_builder.klass.__name__} from default_builder is not a subclass from generic type given by Dependency[{generic_type.__name__}]"
+                )
+
+            if self.builders is None:
+                return
+
+            for key, builder in self.builders.items():
+                if not issubclass(builder.klass, generic_type):
+                    raise TypeError(
+                        f"Dependency: The class {builder.klass.__name__} from builders ({key}) is not a subclass from generic type given by Dependency[{generic_type.__name__}]"
+                    )
+
+    def get_generic_type(self) -> type[T] | None:
+        """
+        Returns type from given generic if exists.
+        """
+        if not hasattr(self, "__orig_class__"):
+            return None
+        return get_args(self.__orig_class__)[0]  # type: ignore
+
+    def get_instance(self) -> T:
         """
         Returns an instance of the dependency.
         """
+
+        self._validate()
+
         if not self.envar_modifier:
             return self.default_builder.build()
 
--- tests/modules/base/application/dependency_injection/unit/test_dependency.py
+++ tests/modules/base/application/dependency_injection/unit/test_dependency.py
@@ -4,76 +4,104 @@ from petisco import Builder, Dependency
 from tests.modules.base.application.dependency_injection.unit.dummy_repositories import (
     InMemoryRepo,
     MyRepo,
+    Repo,
 )
 
 
 @pytest.mark.unit
-def test_dependency_should_return_default_instance():
+class TestDependency:
+    def should_return_default_instance(self):
 
-    dependency = Dependency(
-        name="repo",
-        default_builder=Builder(MyRepo),
-        envar_modifier="REPOSITORY",
-        builders={"inmemory": Builder(InMemoryRepo)},
-    )
+        dependency = Dependency(
+            name="repo",
+            default_builder=Builder(MyRepo),
+            envar_modifier="REPOSITORY",
+            builders={"inmemory": Builder(InMemoryRepo)},
+        )
+        assert isinstance(dependency.get_instance(), MyRepo)
 
-    assert isinstance(dependency.get_instance(), MyRepo)
+    def should_return_one_instance_from_valid_envar_modifier_value(
+        self,
+        monkeypatch,
+    ):
 
+        monkeypatch.setenv("REPOSITORY", "inmemory")
+        dependency = Dependency(
+            name="repo",
+            default_builder=Builder(MyRepo),
+            envar_modifier="REPOSITORY",
+            builders={"inmemory": Builder(InMemoryRepo)},
+        )
 
-@pytest.mark.unit
-def test_dependency_should_return_one_instance_from_valid_envar_modifier_value(
-    monkeypatch,
-):
+        assert isinstance(dependency.get_instance(), InMemoryRepo)
 
-    monkeypatch.setenv("REPOSITORY", "inmemory")
-    dependency = Dependency(
-        name="repo",
-        default_builder=Builder(MyRepo),
-        envar_modifier="REPOSITORY",
-        builders={"inmemory": Builder(InMemoryRepo)},
-    )
+        monkeypatch.undo()
 
-    assert isinstance(dependency.get_instance(), InMemoryRepo)
+    def should_return_default_if_selected_instanced_by_envar_modifier_value_is_not_available(
+        self,
+        monkeypatch,
+    ):
 
-    monkeypatch.undo()
+        monkeypatch.setenv("REPOSITORY", "other")
+        dependency = Dependency(
+            name="repo",
+            default_builder=Builder(MyRepo),
+            envar_modifier="REPOSITORY",
+            builders={"inmemory": Builder(InMemoryRepo)},
+        )
 
+        assert isinstance(dependency.get_instance(), MyRepo)
 
-@pytest.mark.unit
-def test_dependency_should_return_default_if_selected_instanced_by_envar_modifier_value_is_not_available(
-    monkeypatch,
-):
+        monkeypatch.undo()
 
-    monkeypatch.setenv("REPOSITORY", "other")
-    dependency = Dependency(
-        name="repo",
-        default_builder=Builder(MyRepo),
-        envar_modifier="REPOSITORY",
-        builders={"inmemory": Builder(InMemoryRepo)},
-    )
+    def should_return_default_when_optional_parameters_are_not_used(self):
 
-    assert isinstance(dependency.get_instance(), MyRepo)
+        dependency = Dependency(name="repo", default_builder=Builder(MyRepo))
 
-    monkeypatch.undo()
+        assert isinstance(dependency.get_instance(), MyRepo)
 
+    def should_return_default_when_optional_parameters_are_used_but_no_backup_builders_are_defined(
+        self,
+        monkeypatch,
+    ):
 
-@pytest.mark.unit
-def test_dependency_should_return_default_when_optional_parameters_are_not_used():
+        monkeypatch.setenv("REPOSITORY", "other")
+        dependency = Dependency(
+            name="repo", envar_modifier="REPOSITORY", default_builder=Builder(MyRepo)
+        )
 
-    dependency = Dependency(name="repo", default_builder=Builder(MyRepo))
+        assert isinstance(dependency.get_instance(), MyRepo)
 
-    assert isinstance(dependency.get_instance(), MyRepo)
+        monkeypatch.undo()
 
+    def should_return_typed_default_instance(self):
 
-@pytest.mark.unit
-def test_dependency_should_return_default_when_optional_parameters_are_used_but_no_backup_builders_are_defined(
-    monkeypatch,
-):
+        dependency = Dependency[Repo](default_builder=Builder(MyRepo))
 
-    monkeypatch.setenv("REPOSITORY", "other")
-    dependency = Dependency(
-        name="repo", envar_modifier="REPOSITORY", default_builder=Builder(MyRepo)
-    )
+        instance = dependency.get_instance()
 
-    assert isinstance(dependency.get_instance(), MyRepo)
+        assert dependency.get_generic_type() == Repo
+        assert isinstance(instance, MyRepo)
 
-    monkeypatch.undo()
+    def should_raise_error_when_default_builder_type_is_not_a_subclass_of_generic_type(
+        self,
+    ):
+
+        dependency = Dependency[Repo](default_builder=Builder(str))
+
+        with pytest.raises(
+            TypeError,
+            match="Dependency: The class str from default_builder is not a subclass from generic type given by "
+            "Dependency",
+        ):
+            dependency.get_instance()
+
+    def should_raise_error_when_builder_type_is_not_a_subclass_of_generic_type(self):
+
+        dependency = Dependency[Repo](
+            default_builder=Builder(MyRepo),
+            builders={"inmemory": Builder(InMemoryRepo), "invalid": Builder(str)},
+        )
+
+        with pytest.raises(TypeError):
+            dependency.get_instance()
