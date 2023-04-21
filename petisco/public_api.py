ba0042a1eb264a5fd087fb619e31a755a93437f4
acostapazo <acosta@alicebiometrics.com>
2023-04-21 15:53:01 +0200

    feat(controller): add unwrap_result_handler to ease controller configuration (#335)

--- petisco/public_api.py
+++ petisco/public_api.py
@@ -1,7 +1,11 @@
 """Public API of Alice Petisco Framework"""
 from petisco.base.application.application import Application
 from petisco.base.application.application_configurer import ApplicationConfigurer
-from petisco.base.application.controller.controller import Controller, ControllerResult
+from petisco.base.application.controller.controller import (
+    Controller,
+    ControllerResult,
+    result_handler,
+)
 from petisco.base.application.controller.error_map import ErrorMap
 from petisco.base.application.controller.http_error import HttpError
 from petisco.base.application.dependency_injection.container import Container
@@ -91,6 +95,7 @@ __all__ = [
     # base
     "Controller",
     "ControllerResult",
+    "result_handler",
     "ResultMapper",
     "Middleware",
     "NotifierMiddleware",
