e82191595b68888336f2d0a250bd95ee5fdfcf5e
acostapazo <acosta@alicebiometrics.com>
2023-05-16 16:47:33 +0200

    chore(migration): solve error migrating execute_statement in SqlExecutor

--- petisco/extra/sqlalchemy/sql/sql_executor.py
+++ petisco/extra/sqlalchemy/sql/sql_executor.py
@@ -19,7 +19,7 @@ class SqlExecutor:
     def execute_statement(self, statement: str):
         with self.session_scope() as session:
             command = self._get_command(statement)
-            session.execute(text(command))
+            session.execute(command)
 
     def execute_statements(self, statements: List[str]):
         with self.session_scope() as session:
