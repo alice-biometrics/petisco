0023ec8ec85c50cee6d2902fcd83db5498791aa5
acostapazo <acosta@alicebiometrics.com>
2023-05-24 23:38:40 +0200

    fix(tests): message type on rabbitmq_message_queue_name_formatter.py

--- petisco/extra/rabbitmq/application/message/formatter/rabbitmq_message_queue_name_formatter.py
+++ petisco/extra/rabbitmq/application/message/formatter/rabbitmq_message_queue_name_formatter.py
@@ -5,7 +5,11 @@ class RabbitMqMessageQueueNameFormatter:
     @staticmethod
     def format(message: Message, exchange_name: str = None) -> str:
         message_name = message.get_message_name().replace(".", "_")
-        message_type = message.type if message.type != "domain_event" else "event"
+        message_type = (
+            message.get_message_type()
+            if message.get_message_type() != "domain_event"
+            else "event"
+        )
         message_format = (
             f"{message.get_message_version()}.{message_type}.{message_name}"
         )
