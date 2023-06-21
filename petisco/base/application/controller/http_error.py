fc47de381e3844da5480260e39d5dc2c2e0c4816
acostapazo <acosta@alicebiometrics.com>
2023-06-22 00:20:12 +0200

    fix(httperror): fixing error in python3.8

--- petisco/base/application/controller/http_error.py
+++ petisco/base/application/controller/http_error.py
@@ -1,6 +1,4 @@
-from __future__ import annotations
-
-from typing import Any
+from typing import Any, Dict
 
 from pydantic import BaseModel, Field, field_validator
 
@@ -10,7 +8,7 @@ DEFAULT_HTTP_ERROR_DETAIL = "Unknown Error"
 class HttpError(BaseModel):
     status_code: int = Field(default=500)
     detail: str = Field(default=DEFAULT_HTTP_ERROR_DETAIL)
-    headers: dict[str, Any] = Field(default=None)
+    headers: Dict[str, Any] = Field(default=None)
     type_error: str = Field(default=None, validate_default=True)
 
     @field_validator("type_error", mode="before")
