22d262825543f39488f3bd5af6863eff1cadd17e
acostapazo <acosta@alicebiometrics.com>
2023-07-10 11:27:55 +0200

    fix(Middelwares): protect controllers and subscribers from middleware uncontroller exceptions

--- tests/modules/base/domain/messages/unit/test_message_subscriber.py
+++ tests/modules/base/domain/messages/unit/test_message_subscriber.py
@@ -1,66 +1,110 @@
 from typing import List, Type
 
 import pytest
-from meiga import BoolResult, isFailure, isSuccess
-from meiga.assertions import assert_failure, assert_success
+from meiga import AnyResult, BoolResult, isFailure, isSuccess
 
-from petisco import Message, MessageSubscriber, PrintMiddleware
+from petisco import Message, MessageSubscriber, Middleware, PrintMiddleware
 from tests.modules.base.mothers.message_mother import MessageMother
 
 
 @pytest.mark.unit
-def test_message_subscriber_should_construct_and_return_success_on_handle_method():
-    class MyMessageSubscriber(MessageSubscriber):
-        def subscribed_to(self) -> List[Type[Message]]:
-            return [Message]
+class TestMessageSubscriber:
+    def should_construct_and_return_success_on_handle_method(self):  # noqa
+        class MyMessageSubscriber(MessageSubscriber):
+            def subscribed_to(self) -> List[Type[Message]]:
+                return [Message]
 
-        def handle(self, message: Message) -> BoolResult:
-            return isSuccess
+            def handle(self, message: Message) -> BoolResult:
+                return isSuccess
 
-    message_subscriber = MyMessageSubscriber()
+        message_subscriber = MyMessageSubscriber()
 
-    result = message_subscriber.handle(MessageMother.any())
+        result = message_subscriber.handle(MessageMother.any())
 
-    assert_success(result)
+        result.assert_success()
 
+    def should_construct_and_return_failure_on_handle_method(self):  # noqa
+        class MyMessageSubscriber(MessageSubscriber):
+            def subscribed_to(self) -> List[Type[Message]]:
+                return [Message]
 
-@pytest.mark.unit
-def test_message_subscriber_should_construct_and_return_failure_on_handle_method():
-    class MyMessageSubscriber(MessageSubscriber):
-        def subscribed_to(self) -> List[Type[Message]]:
-            return [Message]
+            def handle(self, message: Message) -> BoolResult:
+                return isFailure
 
-        def handle(self, message: Message) -> BoolResult:
-            return isFailure
+        message_subscriber = MyMessageSubscriber()
 
-    message_subscriber = MyMessageSubscriber()
+        result = message_subscriber.handle(MessageMother.any())
 
-    result = message_subscriber.handle(MessageMother.any())
+        result.assert_failure()
 
-    assert_failure(result)
+    @pytest.mark.parametrize(
+        "configured_middlewares",
+        [[PrintMiddleware]],
+    )
+    def should_create_command_input_and_output(
+        self,
+        configured_middlewares,
+    ):
+        class MyMessageSubscriber(MessageSubscriber):
+            class Config:
+                middlewares = configured_middlewares
 
+            def subscribed_to(self) -> List[Type[Message]]:
+                return [Message]
 
-@pytest.mark.unit
-@pytest.mark.parametrize(
-    "configured_middlewares",
-    # [[], [PrintMiddleware], [PrintMiddleware, PrintMiddleware]],
-    [[PrintMiddleware]],
-)
-def test_message_subscriber_should_create_command_input_and_output(
-    configured_middlewares,
-):
-    class MyMessageSubscriber(MessageSubscriber):
-        class Config:
-            middlewares = configured_middlewares
+            def handle(self, message: Message) -> BoolResult:
+                return isSuccess
 
-        def subscribed_to(self) -> List[Type[Message]]:
-            return [Message]
+        message_subscriber = MyMessageSubscriber()
 
-        def handle(self, message: Message) -> BoolResult:
-            return isSuccess
+        result = message_subscriber.handle(MessageMother.any())
 
-    message_subscriber = MyMessageSubscriber()
+        result.assert_success()
 
-    result = message_subscriber.handle(MessageMother.any())
+    def should_not_fail_when_middleware_raise_unexpected_error_on_before(self):  # noqa
+        class RaiseErrorOnBeforeMiddleware(Middleware):
+            def before(self) -> None:
+                raise RuntimeError("Error in before")
 
-    assert_success(result)
+            def after(self, result: AnyResult) -> None:
+                pass
+
+        class MyMessageSubscriber(MessageSubscriber):
+            class Config:
+                middlewares = [RaiseErrorOnBeforeMiddleware]
+
+            def subscribed_to(self) -> List[Type[Message]]:
+                return [Message]
+
+            def handle(self, message: Message) -> BoolResult:
+                return isSuccess
+
+        message_subscriber = MyMessageSubscriber()
+
+        result = message_subscriber.handle(MessageMother.any())
+
+        result.assert_success()
+
+    def should_not_fail_when_middleware_raise_unexpected_error_on_after(self):  # noqa
+        class RaiseErrorOnAfterMiddleware(Middleware):
+            def before(self) -> None:
+                pass
+
+            def after(self, result: AnyResult) -> None:
+                raise RuntimeError("Error in after")
+
+        class MyMessageSubscriber(MessageSubscriber):
+            class Config:
+                middlewares = [RaiseErrorOnAfterMiddleware]
+
+            def subscribed_to(self) -> List[Type[Message]]:
+                return [Message]
+
+            def handle(self, message: Message) -> BoolResult:
+                return isSuccess
+
+        message_subscriber = MyMessageSubscriber()
+
+        result = message_subscriber.handle(MessageMother.any())
+
+        result.assert_success()
