60738c317c1e253a7748adc387f92c6e5d1f504d
acostapazo <acosta@alicebiometrics.com>
2023-07-28 16:46:13 +0200

    chore(persistence): update elastic_is_running_locally (use databases instead of persistence

--- petisco/extra/elastic/elastic_is_running_locally.py
+++ petisco/extra/elastic/elastic_is_running_locally.py
@@ -1,14 +1,14 @@
-from petisco.base.domain.persistence.persistence import Persistence
-from petisco.extra.elastic.legacy_elastic_database import LegacyElasticDatabase
+from petisco.base.domain.persistence.databases import databases
+from petisco.extra.elastic.elastic_database import ElasticDatabase
 
 
 def elastic_is_running_locally() -> bool:
     try:
-        database = LegacyElasticDatabase.local_connection_checker()
-        Persistence().add(database)
-        Persistence().create()
-        is_running_locally = Persistence.is_available(database.name)
+        database = ElasticDatabase.local_connection_checker()
+        databases.add(database)
+        databases.initialize()
+        is_running_locally = databases.get(ElasticDatabase).is_available()
     except:  # noqa: E722
         is_running_locally = False
-    Persistence().remove("test", skip_if_not_exist=True)
+    databases.remove(ElasticDatabase, skip_if_not_exist=True)
     return is_running_locally
