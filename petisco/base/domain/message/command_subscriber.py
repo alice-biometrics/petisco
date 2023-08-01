33bb6858dec7b032e94228d45ea8ea8ec232cbbe
acostapazo <acosta@alicebiometrics.com>
2023-08-01 11:40:44 +0200

    chore(typing): improve message subscribers typing (solve liskov substitution principle)

--- petisco/base/domain/message/command_subscriber.py
+++ petisco/base/domain/message/command_subscriber.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from abc import abstractmethod
+from typing import TypeVar
 
 from meiga import BoolResult
 
@@ -12,6 +13,8 @@ from petisco.base.domain.message.message_subscriber import MessageSubscriber
 # relationship is 1 to 1 and that is why we use Type[Command] instead of List[Type[Command]]
 # and overwrite get_message_subscribers_info base method
 from petisco.base.domain.message.message_subscriber_info import MessageSubscriberInfo
+
+T = TypeVar("T", bound=Command)
 
 
 class CommandSubscriber(MessageSubscriber):
@@ -25,7 +28,7 @@ class CommandSubscriber(MessageSubscriber):
         raise NotImplementedError()
 
     @abstractmethod
-    def handle(self, command: Command) -> BoolResult:  # type: ignore
+    def handle(self, command: T) -> BoolResult:
         raise NotImplementedError()
 
     def get_message_subscribers_info(self) -> list[MessageSubscriberInfo]:
