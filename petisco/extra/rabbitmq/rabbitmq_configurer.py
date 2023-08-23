e004639797a81aa5378242653a1cc1a1ed403b56
acostapazo <acosta@alicebiometrics.com>
2023-08-23 14:19:42 +0200

    feat(RabbitMqConfigurer): add parametrization to configure inner buse… (#406)
    * feat(RabbitMqConfigurer): add parametrization to configure inner buses on consumers
    
    * Update rabbitmq.md
    
    * Update rabbitmq.md
    
    * chore(test-integration): add integration tests for TestRabbitMqConfigurerInnerBuses

--- petisco/extra/rabbitmq/rabbitmq_configurer.py
+++ petisco/extra/rabbitmq/rabbitmq_configurer.py
@@ -7,6 +7,9 @@ from petisco.base.application.dependency_injection.container import Container
 from petisco.base.domain.message.message_configurer import MessageConfigurer
 from petisco.base.domain.message.message_consumer import MessageConsumer
 from petisco.base.domain.message.message_subscriber import MessageSubscriber
+from petisco.extra.rabbitmq.application.message.consumer.rabbitmq_message_consumer import (
+    RabbitMqMessageConsumer,
+)
 
 MAX_RETRIES = 5
 
@@ -24,7 +27,11 @@ class RabbitMqConfigurer(ApplicationConfigurer):
         subscribers: list[type[MessageSubscriber]],
         execute_after_dependencies: bool = True,
         start_consuming: bool = True,
-        alias: str = None,
+        alias: str | None = None,
+        inner_bus_organization: str | None = None,
+        inner_bus_service: str | None = None,
+        clear_subscriber_before: bool | None = None,
+        clear_store_before: bool | None = None,
     ):
         """
         Initializes an instance of RabbitMqConfigurer.
@@ -34,21 +41,45 @@ class RabbitMqConfigurer(ApplicationConfigurer):
             execute_after_dependencies (bool, optional): Flag indicating whether to execute after dependencies. Defaults to True.
             start_consuming (bool, optional): Flag indicating whether to start consuming messages. Defaults to True.
             alias (str, optional): Alias for the MessageConfigurer and MessageConsumer (Container.get(MessageConfigurer|MessageConsumer, alias=self.alias)). Defaults to None.
+            inner_bus_organization (str, optional): configured organization for inner buses (DomainEventBus and CommandBus). If None will configure consumer organization.
+            inner_bus_service (str, optional): configured service for inner buses (DomainEventBus and CommandBus). If None will configure consumer service.
         """
         self.subscribers = subscribers
         self.start_consuming = start_consuming
         self.alias = alias
+        self.inner_bus_organization = inner_bus_organization
+        self.inner_bus_service = inner_bus_service
+        self.consumer: MessageConsumer | None = None
+        self.clear_subscriber_before = (
+            clear_subscriber_before
+            if clear_subscriber_before
+            else CLEAR_SUBSCRIBER_BEFORE
+        )
+        self.clear_store_before = (
+            clear_store_before if clear_store_before else CLEAR_STORE_BEFORE
+        )
+
         super().__init__(execute_after_dependencies)
 
     def execute(self, testing: bool = False) -> None:
         configurer = Container.get(MessageConfigurer, alias=self.alias)
         configurer.configure_subscribers(
             self.subscribers,
-            clear_subscriber_before=CLEAR_SUBSCRIBER_BEFORE,
-            clear_store_before=CLEAR_STORE_BEFORE,
+            clear_subscriber_before=self.clear_subscriber_before,
+            clear_store_before=self.clear_store_before,
         )
 
         if self.start_consuming:
-            consumer = Container.get(MessageConsumer, alias=self.alias)
-            consumer.add_subscribers(self.subscribers)
-            consumer.start()
+            self.consumer = Container.get(MessageConsumer, alias=self.alias)
+
+            if isinstance(self.consumer, RabbitMqMessageConsumer):
+                self.consumer.set_inner_bus_config(
+                    self.inner_bus_organization, self.inner_bus_service
+                )
+
+            self.consumer.add_subscribers(self.subscribers)
+            self.consumer.start()
+
+    def stop(self):
+        if self.consumer:
+            self.consumer.stop()
