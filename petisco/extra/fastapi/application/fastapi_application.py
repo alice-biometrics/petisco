32126f5751a1c6da9a41bbaf699cef269195f86a
acostapazo <acosta@alicebiometrics.com>
2023-09-11 11:19:49 +0200

    feat(ensure-async-routers): check if all routers have been defined as async

--- petisco/extra/fastapi/application/fastapi_application.py
+++ petisco/extra/fastapi/application/fastapi_application.py
@@ -3,10 +3,19 @@ from typing import Callable
 from fastapi import FastAPI
 
 from petisco.base.application.application import Application
+from petisco.extra.fastapi.application.ensure_all_routers_are_async import (
+    ensure_all_routers_are_async,
+)
 
 
 class FastApiApplication(Application):
     fastapi_configurer: Callable[[], FastAPI]
+    ensure_async_routers: bool = False
 
     def get_app(self) -> FastAPI:
-        return self.fastapi_configurer()
+        app = self.fastapi_configurer()
+
+        if self.ensure_async_routers is True:
+            ensure_all_routers_are_async(app)
+
+        return app
