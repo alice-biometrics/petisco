0185445dcde93561cecf9d95230da2943a27cacd
acostapazo <acosta@alicebiometrics.com>
2023-09-27 10:46:39 +0200

    feat(Uuid): researching with new fresh ideas from @fgsalomon

--- examples/fastapi/main.py
+++ examples/fastapi/main.py
@@ -12,12 +12,18 @@ class User(BaseModel):
 
 @app.post("/user")
 def create_user(user: User) -> None:
-    print(user.id)
-    print(type(user.id))
-    print(user.id.value)
+    print(f"{user.id=}")
+    print(f"type {type(user.id)}")
+    print(f"value {user.id.value}")
+
+    print(f"{Uuid.v4()}")
 
 
 @app.get("/user/{user_id}")
 def retrieve_user(user_id: Uuid) -> None:
     print(type(user_id))
+
+    # converted = Uuid(user_id)
+    # print(type(converted))
+
     return None
--- petisco/base/domain/model/uuid.py
+++ petisco/base/domain/model/uuid.py
@@ -5,31 +5,44 @@ from uuid import uuid4
 import validators
 from pydantic import UUID4, GetCoreSchemaHandler
 from pydantic_core import CoreSchema
-from pydantic_core.core_schema import uuid_schema
+from pydantic_core.core_schema import (
+    no_info_after_validator_function,
+    plain_serializer_function_ser_schema,
+    uuid_schema,
+)
 
 from petisco.base.domain.errors.defaults.invalid_uuid import InvalidUuid
 
 USE_LEGACY_UUID = bool(os.getenv("USE_LEGACY_UUID", "False").lower() == "true")
 
 
-class Uuid(UUID4):
+class Uuid(str):
     """
     A base class to define Uuid
 
     Use it to identify domain entities
     """
 
-    def __init__(self, value: str) -> None:
+    def __new__(cls, value: str) -> "Uuid":
         if value is None or not validators.uuid(value):
             raise InvalidUuid(uuid_value=value)
-        super().__init__(str(value))
+        return str.__new__(cls, value)
+
+    def __repr__(self) -> str:
+        return self
+
+    # def __init__(self, value: str) -> None:
+    #     if value is None or not validators.uuid(value):
+    #         raise InvalidUuid(uuid_value=value)
+    #     #super().__init__(str(value))
+    #     self._value = value
 
     @classmethod
     def v4(cls) -> "Uuid":
-        return cls(value=uuid4())
+        return cls(value=str(uuid4()))
 
     def to_str(self) -> str:
-        return str(self)
+        return self
 
     @staticmethod
     def from_str(value: str) -> "Uuid":
@@ -43,13 +56,44 @@ class Uuid(UUID4):
     def from_value(cls, value: str) -> "Uuid":
         return cls(value)
 
+    # @classmethod
+    # def __get_pydantic_core_schema__(
+    #     cls, source: Any, handler: GetCoreSchemaHandler
+    # ) -> CoreSchema:
+    #     return uuid_schema(version=4)
+
     @classmethod
     def __get_pydantic_core_schema__(
         cls, source: Any, handler: GetCoreSchemaHandler
     ) -> CoreSchema:
-        return uuid_schema(version=4)
+        # assert source is Uuid
+        return no_info_after_validator_function(
+            cls._validate,
+            uuid_schema(version=4, strict=False),
+            serialization=plain_serializer_function_ser_schema(
+                cls._serialize,
+                info_arg=False,
+                return_schema=uuid_schema(version=4, strict=False),
+            ),
+        )
+
+    @staticmethod
+    def _validate(value: UUID4) -> "Uuid":
+        return Uuid(str(value))
+        # if isinstance(value, str):
+        #     return Uuid(value
+        #                 )
+        # if isinstance(value, dict):
+        #     return Uuid(value["value"])
+        # else:
+        #     return Uuid(value.value)
+
+    @staticmethod
+    def _serialize(value: "Uuid") -> str:
+        return value.value
 
 
-# if USE_LEGACY_UUID:
-#     from petisco.base.domain.model.legacy_uuid import LegacyUuid #noqa
-#     Uuid = LegacyUuid # noqa
+if USE_LEGACY_UUID:
+    from petisco.base.domain.model.legacy_uuid import LegacyUuid  # noqa
+
+    Uuid = LegacyUuid  # noqa
