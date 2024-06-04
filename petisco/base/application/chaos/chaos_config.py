3d43811eb01eb5cab0e9fd4a7c26e5b46c2f901e
acostapazo <acosta@alicebiometrics.com>
2024-06-04 08:38:04 +0200

    chore(rabbitmq): use annotation instead confloat

--- petisco/base/application/chaos/chaos_config.py
+++ petisco/base/application/chaos/chaos_config.py
@@ -1,28 +1,28 @@
-from typing import Union
+from typing import Annotated, Union
 
-from pydantic import Field, confloat
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
 
 class ChaosConfig(BaseSettings):
-    percentage_invalid_message_publication: float = Field(
-        default=0,
+    percentage_invalid_message_publication: Annotated[float, Field(ge=0.0, le=1.0)] = Field(
+        default=0.0,
         validation_alias="PETISCO_CHAOS_PERCENTAGE_INVALID_MESSAGE_PUBLICATION",
         description="Percentage of invalid message publication. Where 1.0 means rejecting all the publishing messages.",
     )
-    percentage_simulate_nack: confloat(ge=0.0, le=1.0) = Field(
-        default=0,
+    percentage_simulate_nack: Annotated[float, Field(ge=0.0, le=1.0)] = Field(
+        default=0.0,
         validation_alias="PETISCO_CHAOS_PERCENTAGE_SIMULATE_NACK",
         description="Percentage of simulate nack. Where 1.0 rejects all the consuming messages",
     )
-    delay_after_message_consume_seconds: confloat(ge=0.0, le=60.0) = Field(
-        default=0,
+    delay_after_message_consume_seconds: Annotated[float, Field(ge=0.0, le=60.0)] = Field(
+        default=0.0,
         validation_alias="PETISCO_CHAOS_DELAY_AFTER_MESSAGE_CONSUME_SECONDS",
         description="Delay message handler execution for a given number of seconds",
     )
-    percentage_simulate_failures: confloat(ge=0.0, le=1.0) = Field(
-        default=0,
-        validation_alias="PETISCO_CHAOS_DELAY_AFTER_OPERATION_SECONDS",
+    percentage_simulate_failures: Annotated[float, Field(ge=0.0, le=1.0)] = Field(
+        default=0.0,
+        validation_alias="PETISCO_CHAOS_PERCENTAGE_SIMULATE_FAILURES",
         description="Percentage of simulate failures. Where 1.0 simulate always a failure on handlers",
     )
     protected_routing_keys: Union[str, None] = Field(
