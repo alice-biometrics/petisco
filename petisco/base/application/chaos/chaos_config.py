67365273683d45ae9f0171a9f1826d10e0655e0d
acostapazo <acosta@alicebiometrics.com>
2024-06-04 10:09:42 +0200

    fix(rabbitmq): pydantic-setting issue with ChaosConfig (#456)
    * fix(rabbitmq): pydantic-setting issue with ChaosConfig and prepare it to Pydantic 3

--- petisco/base/application/chaos/chaos_config.py
+++ petisco/base/application/chaos/chaos_config.py
@@ -1,38 +1,69 @@
+import sys
 from typing import Union
 
-from pydantic import Field, confloat
+from pydantic import Field
 from pydantic_settings import BaseSettings
 
+if sys.version_info >= (3, 9):
+    from typing import Annotated  # noqa
 
-class ChaosConfig(BaseSettings):
-    percentage_invalid_message_publication: confloat(ge=0.0, le=1.0) = Field(
-        default=0,
-        validation_alias="PETISCO_CHAOS_PERCENTAGE_INVALID_MESSAGE_PUBLICATION",
-        description=(
-            "Percentage of invalid message publication."
-            " Where 1.0 means rejecting all the publishing messages.",
-        ),
-    )
-    percentage_simulate_nack: confloat(ge=0.0, le=1.0) = Field(
-        default=0,
-        validation_alias="PETISCO_CHAOS_PERCENTAGE_SIMULATE_NACK",
-        description="Percentage of simulate nack. Where 1.0 rejects all the consuming messages",
-    )
-    delay_after_message_consume_seconds: confloat(ge=0.0, le=60.0) = Field(
-        default=0,
-        validation_alias="PETISCO_CHAOS_DELAY_AFTER_MESSAGE_CONSUME_SECONDS",
-        description="Delay message handler execution for a given number of seconds",
-    )
-    percentage_simulate_failures: confloat(ge=0.0, le=1.0) = Field(
-        default=0,
-        validation_alias="PETISCO_CHAOS_DELAY_AFTER_OPERATION_SECONDS",
-        description="Percentage of simulate failures. Where 1.0 simulate always a failure on handlers",
-    )
-    protected_routing_keys: Union[str, None] = Field(
-        default=None,
-        validation_alias="PETISCO_CHAOS_PROTECTED_ROUTING_KEYS",
-        description="Routing keys where chaos will not be applied",
-    )
+    # Prepared for Pydantic 3.0
+    class ChaosConfig(BaseSettings):
+        percentage_invalid_message_publication: Annotated[float, Field(ge=0.0, le=1.0)] = Field(
+            default=0.0,
+            validation_alias="PETISCO_CHAOS_PERCENTAGE_INVALID_MESSAGE_PUBLICATION",
+            description="Percentage of invalid message publication. Where 1.0 means rejecting all the publishing messages.",
+        )
+        percentage_simulate_nack: Annotated[float, Field(ge=0.0, le=1.0)] = Field(
+            default=0.0,
+            validation_alias="PETISCO_CHAOS_PERCENTAGE_SIMULATE_NACK",
+            description="Percentage of simulate nack. Where 1.0 rejects all the consuming messages",
+        )
+        delay_after_message_consume_seconds: Annotated[float, Field(ge=0.0, le=60.0)] = Field(
+            default=0.0,
+            validation_alias="PETISCO_CHAOS_DELAY_AFTER_MESSAGE_CONSUME_SECONDS",
+            description="Delay message handler execution for a given number of seconds",
+        )
+        percentage_simulate_failures: Annotated[float, Field(ge=0.0, le=1.0)] = Field(
+            default=0.0,
+            validation_alias="PETISCO_CHAOS_PERCENTAGE_SIMULATE_FAILURES",
+            description="Percentage of simulate failures. Where 1.0 simulate always a failure on handlers",
+        )
+        protected_routing_keys: Union[str, None] = Field(
+            default=None,
+            validation_alias="PETISCO_CHAOS_PROTECTED_ROUTING_KEYS",
+            description="Routing keys where chaos will not be applied",
+        )
+else:
+    # Use deprecated confloat to support Python 3.8 (not available Annotated)
+    from pydantic import confloat  # noqa
+
+    class ChaosConfig(BaseSettings):
+        percentage_invalid_message_publication: confloat(ge=0.0, le=1.0) = Field(
+            default=0.0,
+            validation_alias="PETISCO_CHAOS_PERCENTAGE_INVALID_MESSAGE_PUBLICATION",
+            description="Percentage of invalid message publication. Where 1.0 means rejecting all the publishing messages.",
+        )
+        percentage_simulate_nack: confloat(ge=0.0, le=1.0) = Field(
+            default=0.0,
+            validation_alias="PETISCO_CHAOS_PERCENTAGE_SIMULATE_NACK",
+            description="Percentage of simulate nack. Where 1.0 rejects all the consuming messages",
+        )
+        delay_after_message_consume_seconds: confloat(ge=0.0, le=60.0) = Field(
+            default=0.0,
+            validation_alias="PETISCO_CHAOS_DELAY_AFTER_MESSAGE_CONSUME_SECONDS",
+            description="Delay message handler execution for a given number of seconds",
+        )
+        percentage_simulate_failures: confloat(ge=0.0, le=1.0) = Field(
+            default=0.0,
+            validation_alias="PETISCO_CHAOS_PERCENTAGE_SIMULATE_FAILURES",
+            description="Percentage of simulate failures. Where 1.0 simulate always a failure on handlers",
+        )
+        protected_routing_keys: Union[str, None] = Field(
+            default=None,
+            validation_alias="PETISCO_CHAOS_PROTECTED_ROUTING_KEYS",
+            description="Routing keys where chaos will not be applied",
+        )
 
 
 chaos_config = ChaosConfig()
--- tests/modules/base/application/chaos/unit/test_check_chaos_publication.py
+++ tests/modules/base/application/chaos/unit/test_check_chaos_publication.py
@@ -1,8 +1,9 @@
 from unittest.mock import patch
 
 import pytest
+from pydantic import ValidationError
 
-from petisco.base.application.chaos.chaos_config import chaos_config
+from petisco.base.application.chaos.chaos_config import ChaosConfig, chaos_config
 from petisco.base.application.chaos.check_chaos import (
     ChaosInvalidMessagePublication,
     check_chaos_publication,
@@ -11,6 +12,37 @@ from petisco.base.application.chaos.check_chaos import (
 
 @pytest.mark.unit
 class TestCheckChaosPublication:
+    def should_construct_chaos_config_with_envar_modifications(self, monkeypatch):
+        monkeypatch.setenv("PETISCO_CHAOS_PERCENTAGE_INVALID_MESSAGE_PUBLICATION", "0.1")
+        monkeypatch.setenv("PETISCO_CHAOS_PERCENTAGE_SIMULATE_NACK", "0.2")
+        monkeypatch.setenv("PETISCO_CHAOS_DELAY_AFTER_MESSAGE_CONSUME_SECONDS", "0.3")
+        monkeypatch.setenv("PETISCO_CHAOS_PERCENTAGE_SIMULATE_FAILURES", "0.4")
+        monkeypatch.setenv("PETISCO_CHAOS_PROTECTED_ROUTING_KEYS", "petisco.routing_key")
+
+        chaos_config = ChaosConfig()
+
+        assert chaos_config.percentage_invalid_message_publication == 0.1
+        assert chaos_config.percentage_simulate_nack == 0.2
+        assert chaos_config.delay_after_message_consume_seconds == 0.3
+        assert chaos_config.percentage_simulate_failures == 0.4
+        assert chaos_config.protected_routing_keys == "petisco.routing_key"
+
+    @pytest.mark.parametrize(
+        "env,value",
+        [
+            ("PETISCO_CHAOS_PERCENTAGE_INVALID_MESSAGE_PUBLICATION", "100.0"),
+            ("PETISCO_CHAOS_PERCENTAGE_SIMULATE_NACK", "100.0"),
+            ("PETISCO_CHAOS_DELAY_AFTER_MESSAGE_CONSUME_SECONDS", "100.0"),
+            ("PETISCO_CHAOS_PERCENTAGE_SIMULATE_FAILURES", "100.0"),
+        ],
+    )
+    def should_raise_if_construct_chaos_config_with_invalid_percentage_invalid_message_publication(
+        self, monkeypatch, env, value
+    ):
+        monkeypatch.setenv(env, value)
+        with pytest.raises(ValidationError):
+            ChaosConfig()
+
     def should_check_chaos_publication_without_raising_error(self):  # noqa
         check_chaos_publication()
 
