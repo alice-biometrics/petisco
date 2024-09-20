7be2d5bfaa66b6097c3351235795a8ca310bd59d
acostapazo <acosta@alicebiometrics.com>
2024-09-20 07:24:24 +0200

    feat(threading): update naming

--- petisco/extra/threading/__init__.py
+++ petisco/extra/threading/__init__.py
@@ -1,6 +1,6 @@
+from petisco.extra.threading.concurrent_futures_pool_executor import ConcurrentFuturesPoolExecutor
 from petisco.extra.threading.executable import Executable, ExecutableError
-from petisco.extra.threading.pool_executor import MultiprocessingPoolExecutor, PoolExecutor
-from petisco.extra.threading.thread_pool_executor import ConcurrentFuturesPoolExecutor
+from petisco.extra.threading.multiprocessing_pool_executor import MultiprocessingPoolExecutor, PoolExecutor
 
 threading = [
     "MultiprocessingPoolExecutor",
